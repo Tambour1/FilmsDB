a623256193c261b35f864cd046d33dbe5cf9c4f5
Victor Georges <victorgeorges54@gmail.com>
2024-12-22 23:58:18 +0100

    feat(api): appel à l'API OMDB + affichage des résultats sous forme de liste #2

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,40 @@ export default {
   data() {
     return {
       film: '',
+      apiKey: '2b61feeb',
+      currentResults: [],
+      error: '',
+      isLoading: false,
+      noResults: false,
     };
   },
   methods: {
     onInput(event) {
       this.film = event.target.value;
-      console.log(this.film);
+    },
+
+    async search() { 
+      this.isLoading = true;
+      this.error = '';
+      this.currentResults = [];
+
+      try {
+        const response = await fetch(
+          `https://www.omdbapi.com/?apikey=${this.apiKey}&s=${this.film}`
+        );
+        const data = await response.json();
+
+        if (data.Response === 'True') {
+          this.currentResults = data.Search;
+          this.noResults = this.currentResults.length === 0;
+        } else {
+          this.noResults = true;
+        }
+      } catch (err) {
+        this.error = 'Une erreur est survenue lors de la recherche.';
+      } finally {
+        this.isLoading = false;
+      }
     },
   },
 };
@@ -17,5 +45,19 @@ export default {
 <template>
   <body>
     <input :value="film" @input="onInput" placeholder="Taper le film ici" />
+    <button @click="search">Rechercher</button>
+
+    <p v-if="isLoading">Chargement en cours...</p>
+
+    <p v-if="error" style="color: red;">{{ error }}</p>
+
+    <p v-if="!isLoading && noResults">Aucun résultat trouvé.</p>
+
+    <ul v-if="!isLoading" class="movies">
+      <li v-for="result in currentResults" :key="result.imdbID">
+        <img :src="result.Poster" alt="Poster" />
+        <p>{{ result.Title }} ({{ result.Year }}) - {{ result.Type }}</p>
+      </li>
+    </ul>
   </body>  
 </template>
